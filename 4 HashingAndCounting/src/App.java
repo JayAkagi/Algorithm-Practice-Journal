345a407e8016af8a54d599b707ac6e5b2beed2ea
Chris Bautista <b019782k@gmail.com>
2026-01-22 16:02:23 +0000

    Two sum in hashmaps

--- 4 HashingAndCounting/src/App.java
+++ 4 HashingAndCounting/src/App.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -5,11 +6,15 @@ import java.util.Set;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(firstRepeatingElement("abcda"));
-        System.out.println(firstNonRepeatingElement("aabbcdde"));
+        System.out.println("first repeating char: " + firstRepeatingElement("abcdaec"));
+        System.out.println("first non repeating char: " + firstNonRepeatingElement("aabbcdde"));
+
+        int[] a = {2,7,11,15,5,5,3};
+        System.out.println(Arrays.toString(twoSumHashMap(a, 10)));
     }
 
     public static Character  firstRepeatingElement(String s){
+        System.out.println("\nString: " + s);
         Set<Character> uniquCharacters = new HashSet<>();
         for(Character c : s.toCharArray()){
             if(uniquCharacters.contains(c)){
@@ -18,11 +23,11 @@ public class App {
                 uniquCharacters.add(c);
             } 
         }
-
         return null;
     }
 
     public static Character firstNonRepeatingElement(String s){
+        System.out.println("\nString: " + s);
         Map<Character, Integer> characters = new HashMap<>();
         for(Character c : s.toCharArray()){
             characters.put(c, characters.getOrDefault(c,0) + 1);
@@ -37,7 +42,25 @@ public class App {
                 return key;
             }
         }
-
         return letter;
+    }
+
+    public static int[] twoSumHashMap(int[] a, int target){
+        System.out.println("\nTwo Sum in HashMap");
+        System.out.println(Arrays.toString(a));
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            int x = a[i];
+            int y = target - x;
+
+            if(seen.containsKey(y)){
+                System.out.println("Indeces: ");
+                return new int[] {seen.get(y), i};
+            }
+
+            seen.put(x, i);
+        }
+
+        return null;
     }
 }
